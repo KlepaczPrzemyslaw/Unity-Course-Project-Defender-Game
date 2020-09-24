d5d6b4efb0560303e9c9a293af8c86f2a885e44d
Przemek K Bitbucket <przemyslaw.klepacz.contact@gmail.com>
2020-09-24 23:42:11 +0200

    SourceNodes - Part 1

---
+++ DefenderGame/Assets/Scripts/SceneOrderHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SceneOrderHandler : MonoBehaviour
+{
+	[SerializeField]
+	private bool runOnlyOnce = true;
+
+	[SerializeField]
+	private float offsetForThisPart = 0f;
+
+	private SpriteRenderer spriteRen;
+	private const float precisionMultiplier = 10f;
+
+	void Awake()
+	{
+		spriteRen = GetComponent<SpriteRenderer>();
+	}
+
+	private void LateUpdate()
+	{
+		spriteRen.sortingOrder = -Mathf.RoundToInt(
+			(transform.position.y + offsetForThisPart) * precisionMultiplier);
+
+		if (runOnlyOnce)
+			Destroy(this);
+	}
+}
